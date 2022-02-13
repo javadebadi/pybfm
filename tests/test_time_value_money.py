94ad767ee70bc7cf0591ebcd3e71802d5ed0b246
javadebadi <javad.ebadi.1990@gmail.com>
2022-02-13 23:57:34 +0330

    added tests for time value of money

---
+++ tests/test_time_value_money.py
@@ -0,0 +1,15 @@
+import unittest
+from pybfm.time_value_money import time_value_money
+
+class TimeValueMoney(unittest.TestCase):
+
+    def test_time_value_money(self):
+        self.assertEqual(
+            round(time_value_money(0.01,0,0),2), 0.01
+            )
+        self.assertEqual(
+            round(time_value_money(0.02,0.03,0),4), 0.0506
+            )
+        self.assertEqual(
+            round(time_value_money(0.02,0.03,0.05),4), 0.1031
+        )
